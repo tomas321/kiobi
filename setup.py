80a37c7a942cdcc60edbf7ec611c857ebc7e056a
Tomas Bellus <tomas.bellus@gmail.com>
2019-04-04 14:18:08 +0200

    starting develop

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='kiobi',
+      version='0.0.1rc1',
+      description='import and export Kibana objects',
+      url='http://github.com/tomas321/kiobi',
+      author='Tomas Bellus',
+      author_email='tomas.bellus@gmail.com',
+      packages=find_packages(),
+      # scripts=['path/to/executable/scripts'],
+      install_requires=['requests'])
