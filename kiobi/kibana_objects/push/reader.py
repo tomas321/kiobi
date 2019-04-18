84e96403d86f8a0adfa8d25ca502e3ffff9add9b
Tomas Bellus <tomas.bellus@gmail.com>
2019-04-18 13:35:51 +0200

    remove disallowed fields from saved objects

--- kiobi/kibana_objects/push/reader.py
+++ kiobi/kibana_objects/push/reader.py
@@ -8,4 +8,12 @@ from kiobi.log import log
 def read_objects_file(file_fullpath: str):
     with open(file_fullpath, 'r') as fd:
         log.debug("opened '{}' for reading saved objects json".format(file_fullpath))
-        return fd.read()
+        return __remove_disallowed_fields(fd.read())
+
+
+def __remove_disallowed_fields(data: str):
+    json_data = json.loads(data)
+    for obj in json_data['saved_objects']:
+        if 'updated_at' in obj:
+            del obj['updated_at']
+    return str(json_data)
